db9ce55410d4d86838de69cfb1cb48775ec14895
mcrey fonacier <40071594+polpokpol@users.noreply.github.com>
2019-07-24 00:44:24 +0800

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,54 @@
+public class Node {
+
+    private char character;
+    private Node leftNode;
+    private Node rightNode;
+    private Node middleNode;
+    private int value;
+
+    public Node(char character) {
+        this.character = character;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public char getCharacter() {
+        return character;
+    }
+
+    public Node getMiddleNode() {
+        return middleNode;
+    }
+
+    public void setMiddleNode(Node middleNode) {
+        this.middleNode = middleNode;
+    }
+
+    public Node getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(Node rightNode) {
+        this.rightNode = rightNode;
+    }
+
+    public Node getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(Node leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public void setCharacter(char character) {
+        this.character = character;
+    }
+    
+    
+}
