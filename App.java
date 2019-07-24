7adc8c26e4e5010a65f9c4e685e7767031f5f056
mcrey fonacier <40071594+polpokpol@users.noreply.github.com>
2019-07-25 01:02:56 +0800

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,10 @@
 public class App{
     public static void main(String[] args) {
-        
+        TST tst = new TST();
+
+        tst.put("mcrey", 44);
+        tst.put("fonacier", 2);
+
+        System.out.println(tst.get("ead"));
     }
 }
